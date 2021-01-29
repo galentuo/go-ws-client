b1c24ee3c8cdc53dad5a90a4de8f3eda75a6c55c
jinut2 <jinu.t.92@gmail.com>
2021-01-29 20:16:29 +0530

    basic reader.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/galentuo/go-ws-client
 
 go 1.15
+
+require github.com/gorilla/websocket v1.4.2
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := "wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin"
+	log.Printf("connecting to %s", u)
+
+	c, _, err := websocket.DefaultDialer.Dial(u, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+
+			// Cleanly close the connection by sending a close message and then
+			// waiting (with timeout) for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			return
+		}
+	}
+}
